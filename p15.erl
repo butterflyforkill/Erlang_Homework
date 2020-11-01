6c1ff7e4e822e0cff5e9fb3a729ac07b6bd80f56
butterflyforkill <46646977+butterflyforkill@users.noreply.github.com>
2020-11-01 15:04:45 +0200

    Update p15.erl

--- p15.erl
+++ p15.erl
@@ -3,5 +3,5 @@
 
 replicate(List, Num) -> replicate(List, Num, Num).
 replicate([_H | T], Num, 0) -> replicate(T, Num, Num);
-replicate([H | T], Num, N) -> [H | replicate([H | T], Num, N-1)];
+replicate([H | T], Num, F) -> [H | replicate([H | T], Num, F-1)];
 replicate([],_,_) -> [].
